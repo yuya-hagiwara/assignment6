49e2fe94054aec01e1e12065d95967f8fd499e65
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-08-08 20:32:55 +0900

    プロジェクトをセットアップ”

---
+++ src/main/java/com/tutorial/heIIo/HeIIoApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.heIIo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeIIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeIIoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tutorial/heIIo/HeIIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.heIIo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeIIoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
