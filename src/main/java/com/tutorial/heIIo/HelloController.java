1ca2e08a62f07ae26ad05e66e32efa32d9737d7b
yuya-hagiwara <yuyahagi1990@icloud.com>
2024-08-15 01:47:12 +0900

    追加でHumanのListを返す機能を実装

--- src/main/java/com/tutorial/heIIo/HelloController.java
+++ src/main/java/com/tutorial/heIIo/HelloController.java
@@ -3,12 +3,22 @@ package com.tutorial.heIIo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
 
+@RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-       return "hello world";
+        return "hello world";
+    }
+
+    @GetMapping("/human")
+    public List<Human> gethuman() {
+        List<Human> human = new ArrayList<>();
+        human.add(new Human("yuya", 32));
+        human.add(new Human("taka", 33));
+        return human;
     }
 }
---
+++ src/main/java/com/tutorial/heIIo/Human.java
@@ -0,0 +1,24 @@
+package com.tutorial.heIIo;
+
+public class Human {
+
+
+    private String name;
+
+
+    private int age;
+
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
